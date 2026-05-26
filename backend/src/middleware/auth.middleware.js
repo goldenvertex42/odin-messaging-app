c84bef8499de7583630ffd6628036bb380990642
goldenvertex42 <fabled93@gmail.com>
2026-05-25 19:41:23 -0500

    chore: delete obsolete auth middleware

--- backend/src/middleware/auth.middleware.js
+++
@@ -1,8 +0,0 @@
-export const isAuthenticated = (req, res, next) => {
-  // Passport automatically adds this helper method to the request object
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  
-  return res.status(401).json({ message: 'Unauthorized. Please log in.' });
-};
